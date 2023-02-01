9eab96098b14101e7a790b4b748e2b6690a625b1
Gastón Martínez <gastongmartinez@gmail.com>
2023-02-01 15:16:25 -0300

    Funcion Set-Policy

--- Debloat.psm1
+++ Debloat.psm1
@@ -80,6 +80,81 @@ Function RemoveApps ($Apps) {
 	.NOTES
 	Current user
 #>
+
+function script:Set-Policy {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(
+            Mandatory = $true,
+            Position = 1
+        )]
+        [string]
+        [ValidateSet("Computer", "User")]
+        $Scope,
+
+        [Parameter(
+            Mandatory = $true,
+            Position = 2
+        )]
+        [string]
+        $Path,
+
+        [Parameter(
+            Mandatory = $true,
+            Position = 3
+        )]
+        [string]
+        $Name,
+
+        [Parameter(
+            Mandatory = $true,
+            Position = 4
+        )]
+        [ValidateSet("DWORD", "SZ", "EXSZ", "CLEAR")]
+        [string]
+        $Type,
+
+        [Parameter(
+            Mandatory = $false,
+            Position = 5
+        )]
+        $Value
+    )
+
+    if (-not (Test-Path -Path "$env:SystemRoot\System32\gpedit.msc")) {
+        return
+    }
+
+    switch ($Type) {
+        "CLEAR" {
+            $Policy = @"
+$Scope
+$($Path)
+$($Name)
+$($Type)`n
+"@
+        }
+        default {
+            $Policy = @"
+$Scope
+$($Path)
+$($Name)
+$($Type):$($Value)`n
+"@
+        }
+    }
+
+    if ($Scope -eq "Computer") {
+        $Path = "$env:TEMP\Computer.txt"
+    }
+    else {
+        $Path = "$env:TEMP\User.txt"
+    }
+
+    Add-Content -Path $Path -Value $Policy -Encoding Default -Force
+}
+
 function DiagTrackService {
     param
     (
