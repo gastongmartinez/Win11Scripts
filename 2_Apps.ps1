d0fb3bc131bd8b1be751c6007ea72233a972230e
Gastón Martínez <gastongmartinez@gmail.com>
2023-02-04 15:07:12 -0300

    Taskbar

--- 2_Apps.ps1
+++ 2_Apps.ps1
@@ -85,6 +85,11 @@ Get-ChildItem -Path C:\Users\Public\Desktop -Include *.lnk -File -Recurse | ForE
 # Orden iconos escritorio
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop" -Name FFlags -Value "1075839525"
 
+# Taskbar
+Move-Item -Path "$PSScriptRoot\LayoutModification.xml" -Destination "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"   
+
+((Get-Content -path C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml -Raw) -replace 'Usuario', $env:UserName) | Set-Content -Path C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml
+
 $SN = Read-Host -Prompt "Se recomienda reiniciar el equipo, desea hacerlo ahora? (S/N): "
 if ( $SN -eq "S" ) {
     Restart-Computer
